def43895cff778f8934a38964135692fce726d83
IHC <seele.sans.corpus@gmail.com>
2017-06-30 00:26:41 -0400

    that's probably important

--- projectiles/spawner/npcspawn.lua
+++ projectiles/spawner/npcspawn.lua
@@ -3,5 +3,5 @@ require "/scripts/util.lua"
 function init()
   local configParameter = config.getParameter("scriptConfig")
   local choice = configParameter.options[math.random(#configParameter.options)]
-  world.spawnNpc(entity.position(), choice.species[math.random(#choice.species)], choice.type[math.random(#choice.type)], 1)
+  world.spawnNpc(entity.position(), choice.species[math.random(#choice.species)], choice.type[math.random(#choice.type)], world.threatLevel())
 end
