6eafca6f30ba8c49c6808c461c07dc012d7739e5
IHC <seele.sans.corpus@gmail.com>
2017-07-02 18:42:13 -0400

    error, seed, and parameters handling

--- projectiles/spawner/npcspawn.lua
+++ projectiles/spawner/npcspawn.lua
@@ -3,9 +3,11 @@ require "/scripts/util.lua"
 function init()
   local configParameter = config.getParameter("scriptConfig")
   local i = 1
-  while i < #configParameter.list + 1 do
+  while i <= #configParameter.list do
     local choice = util.randomFromList(configParameter.list[i].options)
-    world.spawnNpc(entity.position(), util.randomFromList(choice.species), util.randomFromList(choice.type), world.threatLevel())
+    if choice.species and choice.type then
+      world.spawnNpc(entity.position(), util.randomFromList(choice.species), util.randomFromList(choice.type), world.threatLevel(), choice.seed, { scriptConfig = choice.parameters or {}})
+    end
     i = i + 1
   end
 end
