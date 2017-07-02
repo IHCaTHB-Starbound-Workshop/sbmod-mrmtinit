65bd6f3347400a0ba8aaddfd6b5c9ef4d528df35
IHC <seele.sans.corpus@gmail.com>
2017-07-02 17:37:46 -0400

    add list to projectile lua

--- projectiles/spawner/npcspawn.lua
+++ projectiles/spawner/npcspawn.lua
@@ -2,6 +2,10 @@ require "/scripts/util.lua"
 
 function init()
   local configParameter = config.getParameter("scriptConfig")
-  local choice = util.randomFromList(configParameter.options)
-  world.spawnNpc(entity.position(), util.randomFromList(choice.species), util.randomFromList(choice.type), world.threatLevel())
+  local i = 1
+  while i < #configParameter.list + 1 do
+    local choice = util.randomFromList(configParameter.list[i].options)
+    world.spawnNpc(entity.position(), util.randomFromList(choice.species), util.randomFromList(choice.type), world.threatLevel())
+    i = i + 1
+  end
 end
